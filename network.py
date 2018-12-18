f1314920e0a41ab1b220e2fad3da8e379d64feb9
furuya1223 <kouki23.bass@gmail.com>
2018-12-18 23:30:23 +0900

    change layer num from 2 to 3

--- network.py
+++ network.py
@@ -13,10 +13,13 @@ class Classifier(nn.Module):
         # モデル定義
         self.conv1_1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)   # 畳み込み層
         self.conv1_2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv1_3 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
         self.conv2_1 = nn.Conv2d(32, 64, 3, stride=1, padding=1)   # 畳み込み層
         self.conv2_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv2_3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
         self.conv3_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)   # 畳み込み層
         self.conv3_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv3_3 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
         self.drop1 = nn.Dropout2d()
         self.fc1 = nn.Linear(16*16*128, 500)     # 全結合層
         self.drop2 = nn.Dropout()
@@ -25,12 +28,15 @@ class Classifier(nn.Module):
     def forward(self, x):
         x = F.relu(self.conv1_1(x))
         x = F.relu(self.conv1_2(x))
+        x = F.relu(self.conv1_3(x))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（128->64）
         x = F.relu(self.conv2_1(x))
         x = F.relu(self.conv2_2(x))
+        x = F.relu(self.conv2_3(x))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（64->32）
         x = F.relu(self.conv3_1(x))
         x = F.relu(self.conv3_2(x))
+        x = F.relu(self.conv3_3(x))
         x = F.max_pool2d(self.drop1(x), 2, 2)  # 縦横ともに半分になる（32->16）
         x = x.view(-1, 16*16*128)
         x = self.drop2(F.relu(self.fc1(x)))
