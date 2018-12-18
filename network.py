7cb2fb6cd0115c2329f2a27d60836f6adaa2b390
furuya1223 <kouki23.bass@gmail.com>
2018-12-19 00:00:30 +0900

    add learning rate option

--- network.py
+++ network.py
@@ -13,25 +13,31 @@ class Classifier(nn.Module):
         # モデル定義
         self.conv1_1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)   # 畳み込み層
         self.conv1_2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv1_3 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
         self.bn1 = nn.BatchNorm2d(32)
         self.conv2_1 = nn.Conv2d(32, 64, 3, stride=1, padding=1)   # 畳み込み層
         self.conv2_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv2_3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
         self.bn2 = nn.BatchNorm2d(64)
         self.conv3_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)   # 畳み込み層
         self.conv3_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
+        self.conv3_3 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
         self.bn3 = nn.BatchNorm2d(128)
         self.fc1 = nn.Linear(16*16*128, 500)     # 全結合層
         self.fc2 = nn.Linear(500, 6)         # 全結合層
 
     def forward(self, x):
         x = F.relu(self.conv1_1(x))
-        x = F.relu(self.bn1(self.conv1_2(x)))
+        x = F.relu(self.conv1_2(x))
+        x = F.relu(self.bn1(self.conv1_3(x)))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（128->64）
         x = F.relu(self.conv2_1(x))
-        x = F.relu(self.bn2(self.conv2_2(x)))
+        x = F.relu(self.conv2_2(x))
+        x = F.relu(self.bn2(self.conv2_3(x)))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（64->32）
         x = F.relu(self.conv3_1(x))
-        x = F.relu(self.bn3(self.conv3_2(x)))
+        x = F.relu(self.conv3_2(x))
+        x = F.relu(self.bn3(self.conv3_3(x)))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（32->16）
         x = x.view(-1, 16*16*128)
         x = F.relu(self.fc1(x))
