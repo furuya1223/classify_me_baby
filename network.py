3f24c3be47731a9cad60eb25dddfb7ee37efef17
furuya1223 <kouki23.bass@gmail.com>
2018-12-19 10:39:11 +0900

    reduce network complexity

--- network.py
+++ network.py
@@ -11,35 +11,17 @@ class Classifier(nn.Module):
     def __init__(self):
         super(Classifier, self).__init__()  # おまじない（親クラスのコンストラクタを明示的に呼ばないといけない）
         # モデル定義
-        self.conv1_1 = nn.Conv2d(3, 32, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv1_2 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv1_3 = nn.Conv2d(32, 32, 3, stride=1, padding=1)   # 畳み込み層
-        self.bn1 = nn.BatchNorm2d(32)
-        self.conv2_1 = nn.Conv2d(32, 64, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv2_2 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv2_3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)   # 畳み込み層
-        self.bn2 = nn.BatchNorm2d(64)
-        self.conv3_1 = nn.Conv2d(64, 128, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv3_2 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
-        self.conv3_3 = nn.Conv2d(128, 128, 3, stride=1, padding=1)   # 畳み込み層
-        self.bn3 = nn.BatchNorm2d(128)
-        self.fc1 = nn.Linear(16*16*128, 500)     # 全結合層
-        self.fc2 = nn.Linear(500, 6)         # 全結合層
+        self.conv1_1 = nn.Conv2d(3, 32, 5, stride=1, padding=2)   # 畳み込み層
+        self.conv2_1 = nn.Conv2d(32, 64, 5, stride=1, padding=2)   # 畳み込み層
+        self.fc1 = nn.Linear(32*32*128, 1024)     # 全結合層
+        self.fc2 = nn.Linear(1024, 6)         # 全結合層
 
     def forward(self, x):
         x = F.relu(self.conv1_1(x))
-        x = F.relu(self.conv1_2(x))
-        x = F.relu(self.bn1(self.conv1_3(x)))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（128->64）
         x = F.relu(self.conv2_1(x))
-        x = F.relu(self.conv2_2(x))
-        x = F.relu(self.bn2(self.conv2_3(x)))
         x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（64->32）
-        x = F.relu(self.conv3_1(x))
-        x = F.relu(self.conv3_2(x))
-        x = F.relu(self.bn3(self.conv3_3(x)))
-        x = F.max_pool2d(x, 2, 2)  # 縦横ともに半分になる（32->16）
-        x = x.view(-1, 16*16*128)
+        x = x.view(-1, 32*32*128)
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return F.softmax(x, dim=1)
